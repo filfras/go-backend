fdb3f1fc65aa3ade838fd07180ac39638b9b68a8
Filipa <118170276+filfras@users.noreply.github.com>
2022-12-20 23:37:41 +0000

    commit from 20 12 2022

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-//it will contain our express app
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Thing = require ('./models/thing');
+
 const app = express();
 //running express to create our app
+
+mongoose.connect('mongodb+srv://filipa:RKpdUJfS7ZLnmj5S@cluster0.xyab63i.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error('error');
+    });
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); //allow any request
@@ -18,39 +28,111 @@ app.use(bodyParser.json());
 //This tells Express to take any incoming request that has Content-Type  application/json  and make its  body  available on the  req  object, 
 //allowing you to write the following POST middleware:
 
+//save route; our POST route:
 app.post('/api/stuff', (req, res, next) => {
     console.log(req.body);
-    //we will receive the jason response by the front-end by using the req.body
-    res.status(201).json({
-        message: 'Thing created successfully!'
+    //we will receive the json response by the front-end by using the req.body
+    
+    const thing = new Thing({
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
     });
+    thing.save().then( //save it to the database
+        () => {
+            res.status(201).json({ //status 201 = sucessfully
+                message: 'Post saved succefully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({ //status 400 = error
+                error: error
+            });
+        }
+    )
 });
 
-//check app.use (#)
-//This is a piece of middleware; added "next" argument so you can send one response to the next middle ware by finishing with next();
-//app.get only intersepts get requests
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-    {
-        _id: 'oeihfzeoi',
-        title: 'My first thing',
-        description: 'All of the info about my first thing',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Hasselblad_500_CM.jpg/1024px-Hasselblad_500_CM.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-    },
-    {
-        _id: 'oeihfzeomoihi',
-        title: 'My second thing',
-        description: 'All of the info about my second thing',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Hasselblad_500_CM.jpg/1024px-Hasselblad_500_CM.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-    },
-    ];
-    res.status(200).json(stuff);
-    //res.json({ message: 'Your request was successful!' }); 
+//app.get only responds to get responses
+app.get('/api/stuff/:id', (req, res, next) => {
+//:id means that this is a dynamic parameter 
+    Thing.findOne({
+        _id: req.params.id
+    }).then(
+        (thing) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
 });
+
+//update-modify it is a put request
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+    });
+    Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+        res.status(201).json({
+        message: 'Thing updated successfully!'
+        });
+    }
+    ).catch(
+    (error) => {
+        res.status(400).json({
+        error: error
+        });
+    }
+    );
+});
+
+//route to delete the thing
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+        res.status(200).json({
+        message: 'Deleted!'
+        });
+    }
+    ).catch(
+    (error) => {
+        res.status(400).json({
+        error: error
+        });
+    }
+    );
+});
+
+//This is a piece of middleware; added "next" argument so you can send one response to the next middle ware by finishing with next();
+
+ //we are going ot use the "thing" model we created
+//and then use the "find" method
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find().then(
+        (things) => {
+        res.status(200).json(things);
+        }
+    ).catch(
+        (error) => {
+        res.status(400).json({
+            error: error
+        });
+        }
+    );
+    });
+//enfoce a data schema on our MongoDB -> Models
 
 module.exports = app;
 //access outside this js file
